a4e3f990428437c9fcf91274b4a9c65911adb066
Johannes <johannes.coolen@hotmail.com>
2021-05-05 09:16:18 +0200

    fixed sigfox lib

--- lib/sigfox.py
+++ lib/sigfox.py
@@ -2,7 +2,7 @@ from network import Sigfox
 import socket
 import ubinascii
 
-def send(humidity,distance):
+def send(humidity,distance,temperature):
     # init Sigfox for RCZ1 (Europe)
     sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 
@@ -21,4 +21,4 @@ def send(humidity,distance):
     print(rest)
     # send some bytes
     #s.send(string)
-    s.send(bytes([humidity,factor,rest]))
+    s.send(bytes([humidity,factor,rest,temperature]))
